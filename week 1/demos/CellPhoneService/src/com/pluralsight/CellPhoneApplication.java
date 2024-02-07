a9ba49c91cb5e1e2018021d8b4bb56c342bd165d
TeeeCodes <Terencepriesterr@gmail.com>
2024-02-07 13:42:46 -0500

    week1 demos

---
+++ week 1/demos/CellPhoneService/src/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public void main() {
+
+    }
+    public static void main(String[] args) {
+        CellPhone phoneInstance = new CellPhone();
+
+//        User Iput Scanner
+        Scanner userScanner = new Scanner(System.in);
+
+//        Set setter methods
+        System.out.println("What is the serial number?");
+
+    }
+}
